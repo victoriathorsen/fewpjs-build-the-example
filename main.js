a7a0f903ccf73dfa2df6d2bafce9419f9ec18796
Victoria <victoriathorsenn@gmail.com>
2021-04-30 21:54:20 -0400

    commit tree 57897fb86b822d665a085b545d1e244826553380

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
 // Defining text characters for the empty and full hearts for you to use later.
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+const glyphStates = {
+  "♡" : "♥",
+  "♥" : "♡"
+};
+
+const colorHeart = {
+  "red" : "",
+  "" : "red"
+};
+
 // Your JavaScript code goes here!
 
+let hearts = document.querySelectorAll('.like-glyph');
 
 
+function handleClick(e) {
+  let heart = e.target
+  mimicServerCall()
+  .then(function(serverMessage){
+    heart.innerText = glyphStates[heart.innerText]
+    heart.style.color = colorHeart[heart.style.color]
+    }
+  )
+  .catch(function(error) {
+    const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+  });
+}
 
+
+for (let h of hearts){
+  h.addEventListener('click', handleClick)
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
